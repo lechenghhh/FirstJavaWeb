a6a3ad402edf0116a7a474a9164c558b99577e10
Cheng <lechenggood@foxmail.com>
2018-10-13 17:49:37 +0800

    ueditor 5

--- web/13UEditor/umeditor.config.js
+++ web/13UEditor/umeditor.config.js
@@ -19,7 +19,7 @@
      * 因此，UEditor提供了针对不同页面的编辑器可单独配置的根路径，具体来说，在需要实例化编辑器的页面最顶部写上如下代码即可。当然，需要令此处的URL等于对应的配置。
      * window.UMEDITOR_HOME_URL = "/xxxx/xxxx/";
      */
-    var URL = window.UMEDITOR_HOME_URL || (function(){
+    var URL = window.UMEDITOR_HOME_URL || (function () {
 
         function PathStack() {
 
@@ -33,83 +33,83 @@
             this.path = this.documentURL;
             this.stack = [];
 
-            this.push( this.documentURL );
+            this.push(this.documentURL);
 
         }
 
-        PathStack.isParentPath = function( path ){
+        PathStack.isParentPath = function (path) {
             return path === '..';
         };
 
-        PathStack.hasProtocol = function( path ){
-            return !!PathStack.getProtocol( path );
+        PathStack.hasProtocol = function (path) {
+            return !!PathStack.getProtocol(path);
         };
 
-        PathStack.getProtocol = function( path ){
+        PathStack.getProtocol = function (path) {
 
-            var protocol = /^[^:]*:\/*/.exec( path );
+            var protocol = /^[^:]*:\/*/.exec(path);
 
             return protocol ? protocol[0] : null;
 
         };
 
         PathStack.prototype = {
-            push: function( path ){
+            push: function (path) {
 
                 this.path = path;
 
-                update.call( this );
-                parse.call( this );
+                update.call(this);
+                parse.call(this);
 
                 return this;
 
             },
-            getPath: function(){
+            getPath: function () {
                 return this + "";
             },
-            toString: function(){
-                return this.protocol + ( this.stack.concat( [''] ) ).join( this.separator );
+            toString: function () {
+                return this.protocol + (this.stack.concat([''])).join(this.separator);
             }
         };
 
         function update() {
 
-            var protocol = PathStack.getProtocol( this.path || '' );
+            var protocol = PathStack.getProtocol(this.path || '');
 
-            if( protocol ) {
+            if (protocol) {
 
                 //根协议
                 this.protocol = protocol;
 
                 //local
-                this.localSeparator = /\\|\//.exec( this.path.replace( protocol, '' ) )[0];
+                this.localSeparator = /\\|\//.exec(this.path.replace(protocol, ''))[0];
 
                 this.stack = [];
             } else {
-                protocol = /\\|\//.exec( this.path );
+                protocol = /\\|\//.exec(this.path);
                 protocol && (this.localSeparator = protocol[0]);
             }
 
         }
 
-        function parse(){
+        function parse() {
 
-            var parsedStack = this.path.replace( this.currentDirPattern, '' );
+            var parsedStack = this.path.replace(this.currentDirPattern, '');
 
-            if( PathStack.hasProtocol( this.path ) ) {
-                parsedStack = parsedStack.replace( this.protocol , '');
+            if (PathStack.hasProtocol(this.path)) {
+                parsedStack = parsedStack.replace(this.protocol, '');
             }
 
-            parsedStack = parsedStack.split( this.localSeparator );
+            parsedStack = parsedStack.split(this.localSeparator);
             parsedStack.length = parsedStack.length - 1;
 
-            for(var i= 0,tempPath,l=parsedStack.length,root = this.stack;i<l;i++){
+            for (var i = 0, tempPath, l = parsedStack.length, root = this.stack; i < l; i++) {
                 tempPath = parsedStack[i];
-                if(tempPath){
-                    if( PathStack.isParentPath( tempPath ) ) {
+                if (tempPath) {
+                    if (PathStack.isParentPath(tempPath)) {
                         root.pop();
                     } else {
-                        root.push( tempPath );
+                        root.push(tempPath);
                     }
                 }
 
@@ -120,9 +120,9 @@
 
         var currentPath = document.getElementsByTagName('script');
 
-        currentPath = currentPath[ currentPath.length -1 ].src;
+        currentPath = currentPath[currentPath.length - 1].src;
 
-        return new PathStack().push( currentPath ) + "";
+        return new PathStack().push(currentPath) + "";
 
 
     })();
@@ -133,18 +133,18 @@
     window.UMEDITOR_CONFIG = {
 
         //为编辑器实例添加一个路径，这个不能被注释
-        UMEDITOR_HOME_URL : URL
+        UMEDITOR_HOME_URL: URL
 
         //图片上传配置区
-        ,imageUrl:URL+"jsp/imageUp.jsp"             //图片上传提交地址
-        ,imagePath:URL + "jsp/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
-        ,imageFieldName:"upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
+        , imageUrl: URL + "jsp/imageUp.jsp"             //图片上传提交地址
+        , imagePath: URL + "jsp/"                     //图片修正地址，引用了fixedImagePath,如有特殊需求，可自行配置
+        , imageFieldName: "upfile"                   //图片数据的key,若此处修改，需要在后台对应文件修改对应参数
 
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
-        ,toolbar:[
+        , toolbar: [
             'source | undo redo | bold italic underline strikethrough | superscript subscript | forecolor backcolor | removeformat |',
-            'insertorderedlist insertunorderedlist | selectall cleardoc paragraph | fontfamily fontsize' ,
+            'insertorderedlist insertunorderedlist | selectall cleardoc paragraph | fontfamily fontsize',
             '| justifyleft justifycenter justifyright justifyjustify |',
             'link unlink | emotion image video  | map',
             '| horizontal print preview fullscreen', 'drafts', 'formula'
@@ -152,11 +152,11 @@
 
         //语言配置项,默认是zh-cn。有需要的话也可以使用如下这样的方式来自动多语言切换，当然，前提条件是lang文件夹下存在对应的语言文件：
         //lang值也可以通过自动获取 (navigator.language||navigator.browserLanguage ||navigator.userLanguage).toLowerCase()
-        //,lang:"zh-cn"
-        //,langPath:URL +"lang/"
+        , lang: "zh-cn"
+        , langPath: URL + "lang/"
 
         //ie下的链接自动监测
-        //,autourldetectinie:false
+        , autourldetectinie: false
 
         //主题配置项,默认是default。有需要的话也可以使用如下这样的方式来自动多主题切换，当然，前提条件是themes文件夹下存在对应的主题文件：
         //现有如下皮肤:default
@@ -164,9 +164,8 @@
         //,themePath:URL +"themes/"
 
 
-
         //针对getAllHtml方法，会在对应的head标签中增加该编码设置。
-        //,charset:"utf-8"
+        , charset: "utf-8"
 
         //常用配置项目
         //,isShow : true    //默认显示编辑器
@@ -202,17 +201,17 @@
 
         //fontfamily
         //字体设置
-//        ,'fontfamily':[
-//              { name: 'songti', val: '宋体,SimSun'},
-//          ]
+        , 'fontfamily': [
+            {name: 'songti', val: '宋体,SimSun'},
+        ]
 
         //fontsize
         //字号
-        //,'fontsize':[10, 11, 12, 14, 16, 18, 20, 24, 36]
+        , 'fontsize': [10, 11, 12, 14, 16, 18, 20, 24, 36, 48]
 
         //paragraph
         //段落格式 值留空时支持多语言自动识别，若配置，则以配置值为准
-        //,'paragraph':{'p':'', 'h1':'', 'h2':'', 'h3':'', 'h4':'', 'h5':'', 'h6':''}
+        , 'paragraph': {'p': '', 'h1': '', 'h2': '', 'h3': '', 'h4': '', 'h5': '', 'h6': ''}
 
         //undo
         //可以最多回退的次数,默认20
@@ -232,9 +231,9 @@
         // 是否允许粘贴QQ截屏，上传并插入,默认true
         //,pasteImageEnabled:true
 
-        //autoHeightEnabled
+        // autoHeightEnabled
         // 是否自动长高,默认true
-        //,autoHeightEnabled:true
+        // ,autoHeightEnabled:true
 
         //autoFloatEnabled
         //是否保持toolbar的位置不动,默认true
@@ -244,81 +243,81 @@
         //,topOffset:30
 
         //填写过滤规则
-        ,filterRules: {}
+        , filterRules: {}
         // xss 过滤是否开启,inserthtml等操作
- 		,xssFilterRules: true
- 		//input xss过滤
- 		,inputXssFilter: true
- 		//output xss过滤
- 		,outputXssFilter: true
- 		// xss过滤白名单 名单来源: https://raw.githubusercontent.com/leizongmin/js-xss/master/lib/default.js
-        ,whiteList: {
-            a:      ['target', 'href', 'title', 'style', 'class', 'id'],
-			abbr:   ['title', 'style', 'class', 'id'],
-			address: ['style', 'class', 'id'],
-			area:   ['shape', 'coords', 'href', 'alt', 'style', 'class', 'id'],
-			article: ['style', 'class', 'id'],
-			aside:  ['style', 'class', 'id'],
-			audio:  ['autoplay', 'controls', 'loop', 'preload', 'src', 'style', 'class', 'id'],
-			b:      ['style', 'class', 'id'],
-			bdi:    ['dir'],
-			bdo:    ['dir'],
-			big:    [],
-			blockquote: ['cite', 'style', 'class', 'id'],
-			br:     [],
-			caption: ['style', 'class', 'id'],
-			center: [],
-			cite:   [],
-			code:   ['style', 'class', 'id'],
-			col:    ['align', 'valign', 'span', 'width', 'style', 'class', 'id'],
-			colgroup: ['align', 'valign', 'span', 'width', 'style', 'class', 'id'],
-			dd:     ['style', 'class', 'id'],
-			del:    ['datetime', 'style', 'class', 'id'],
-			details: ['open', 'style', 'class', 'id'],
-			div:    ['style', 'class', 'id'],
-			dl:     ['style', 'class', 'id'],
-			dt:     ['style', 'class', 'id'],
-			em:     ['style', 'class', 'id'],
-            embed:  ['style', 'class', 'id', '_url', 'type', 'pluginspage', 'src', 'width', 'height', 'wmode', 'play', 'loop', 'menu', 'allowscriptaccess', 'allowfullscreen'],
-			font:   ['color', 'size', 'face', 'style', 'class', 'id'],
-			footer: ['style', 'class', 'id'],
-			h1:     ['style', 'class', 'id'],
-			h2:     ['style', 'class', 'id'],
-			h3:     ['style', 'class', 'id'],
-			h4:     ['style', 'class', 'id'],
-			h5:     ['style', 'class', 'id'],
-			h6:     ['style', 'class', 'id'],
-			header: ['style', 'class', 'id'],
-			hr:     ['style', 'class', 'id'],
-			i:      ['style', 'class', 'id'],
+        , xssFilterRules: false
+        //input xss过滤
+        , inputXssFilter: false
+        //output xss过滤
+        , outputXssFilter: false
+        // xss过滤白名单 名单来源: https://raw.githubusercontent.com/leizongmin/js-xss/master/lib/default.js
+        , whiteList: {
+            a: ['target', 'href', 'title', 'style', 'class', 'id'],
+            abbr: ['title', 'style', 'class', 'id'],
+            address: ['style', 'class', 'id'],
+            area: ['shape', 'coords', 'href', 'alt', 'style', 'class', 'id'],
+            article: ['style', 'class', 'id'],
+            aside: ['style', 'class', 'id'],
+            audio: ['autoplay', 'controls', 'loop', 'preload', 'src', 'style', 'class', 'id'],
+            b: ['style', 'class', 'id'],
+            bdi: ['dir'],
+            bdo: ['dir'],
+            big: [],
+            blockquote: ['cite', 'style', 'class', 'id'],
+            br: [],
+            caption: ['style', 'class', 'id'],
+            center: [],
+            cite: [],
+            code: ['style', 'class', 'id'],
+            col: ['align', 'valign', 'span', 'width', 'style', 'class', 'id'],
+            colgroup: ['align', 'valign', 'span', 'width', 'style', 'class', 'id'],
+            dd: ['style', 'class', 'id'],
+            del: ['datetime', 'style', 'class', 'id'],
+            details: ['open', 'style', 'class', 'id'],
+            div: ['style', 'class', 'id'],
+            dl: ['style', 'class', 'id'],
+            dt: ['style', 'class', 'id'],
+            em: ['style', 'class', 'id'],
+            embed: ['style', 'class', 'id', '_url', 'type', 'pluginspage', 'src', 'width', 'height', 'wmode', 'play', 'loop', 'menu', 'allowscriptaccess', 'allowfullscreen'],
+            font: ['color', 'size', 'face', 'style', 'class', 'id'],
+            footer: ['style', 'class', 'id'],
+            h1: ['style', 'class', 'id'],
+            h2: ['style', 'class', 'id'],
+            h3: ['style', 'class', 'id'],
+            h4: ['style', 'class', 'id'],
+            h5: ['style', 'class', 'id'],
+            h6: ['style', 'class', 'id'],
+            header: ['style', 'class', 'id'],
+            hr: ['style', 'class', 'id'],
+            i: ['style', 'class', 'id'],
             iframe: ['style', 'class', 'id', 'src', 'frameborder', 'data-latex'],
-			img:    ['src', 'alt', 'title', 'width', 'height', 'style', 'class', 'id', '_url'],
-			ins:    ['datetime', 'style', 'class', 'id'],
-			li:     ['style', 'class', 'id'],
-			mark:   [],
-			nav:    [],
-			ol:     ['style', 'class', 'id'],
-			p:      ['style', 'class', 'id'],
-			pre:    ['style', 'class', 'id'],
-			s:      [],
-			section:[],
-			small:  ['style', 'class', 'id'],
-			span:   ['style', 'class', 'id'],
-			sub:    ['style', 'class', 'id'],
-			sup:    ['style', 'class', 'id'],
-			strong: ['style', 'class', 'id'],
-			table:  ['width', 'border', 'align', 'valign', 'style', 'class', 'id'],
-			tbody:  ['align', 'valign', 'style', 'class', 'id'],
-			td:     ['width', 'rowspan', 'colspan', 'align', 'valign', 'style', 'class', 'id'],
-			tfoot:  ['align', 'valign', 'style', 'class', 'id'],
-			th:     ['width', 'rowspan', 'colspan', 'align', 'valign', 'style', 'class', 'id'],
-			thead:  ['align', 'valign', 'style', 'class', 'id'],
-			tr:     ['rowspan', 'align', 'valign', 'style', 'class', 'id'],
-			tt:     ['style', 'class', 'id'],
-			u:      [],
-			ul:     ['style', 'class', 'id'],
-            svg:    ['style', 'class', 'id', 'width', 'height', 'xmlns', 'fill', 'viewBox'],
-			video:  ['autoplay', 'controls', 'loop', 'preload', 'src', 'height', 'width', 'style', 'class', 'id']
+            img: ['src', 'alt', 'title', 'width', 'height', 'style', 'class', 'id', '_url'],
+            ins: ['datetime', 'style', 'class', 'id'],
+            li: ['style', 'class', 'id'],
+            mark: [],
+            nav: [],
+            ol: ['style', 'class', 'id'],
+            p: ['style', 'class', 'id'],
+            pre: ['style', 'class', 'id'],
+            s: [],
+            section: [],
+            small: ['style', 'class', 'id'],
+            span: ['style', 'class', 'id'],
+            sub: ['style', 'class', 'id'],
+            sup: ['style', 'class', 'id'],
+            strong: ['style', 'class', 'id'],
+            table: ['width', 'border', 'align', 'valign', 'style', 'class', 'id'],
+            tbody: ['align', 'valign', 'style', 'class', 'id'],
+            td: ['width', 'rowspan', 'colspan', 'align', 'valign', 'style', 'class', 'id'],
+            tfoot: ['align', 'valign', 'style', 'class', 'id'],
+            th: ['width', 'rowspan', 'colspan', 'align', 'valign', 'style', 'class', 'id'],
+            thead: ['align', 'valign', 'style', 'class', 'id'],
+            tr: ['rowspan', 'align', 'valign', 'style', 'class', 'id'],
+            tt: ['style', 'class', 'id'],
+            u: [],
+            ul: ['style', 'class', 'id'],
+            svg: ['style', 'class', 'id', 'width', 'height', 'xmlns', 'fill', 'viewBox'],
+            video: ['autoplay', 'controls', 'loop', 'preload', 'src', 'height', 'width', 'style', 'class', 'id']
         }
     };
 })();
